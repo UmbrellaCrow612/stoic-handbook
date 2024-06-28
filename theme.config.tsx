fdb9a806bd74ae7f84f0e7598920111ab50c5caa
Yousaf Wazir <umbrellaman612@gmail.com>
2024-06-28 12:11:46 +0100

    started SEO stuff

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,17 +1,55 @@
 import React from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
   logo: <span>Stoic Handbook</span>,
   project: {
     link: "https://github.com/UmbrellaCrow612/stoic-handbook",
   },
-  docsRepositoryBase: "https://github.com/UmbrellaCrow612/stoic-handbook/tree/main",
+  docsRepositoryBase:
+    "https://github.com/UmbrellaCrow612/stoic-handbook/tree/main",
   footer: {
     text: "Nextra Docs Template",
   },
   sidebar: {
     toggleButton: true,
+  },
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
+      return {
+        titleTemplate: "%s - Stoic Handbook",
+      };
+    }
+  },
+  head: () => {
+    const { asPath, defaultLocale, locale } = useRouter();
+    const { frontMatter } = useConfig();
+
+    const origin =
+      typeof window !== "undefined" && window.location.origin
+        ? window.location.origin
+        : "";
+
+    const URL = `${origin}${asPath}`;
+
+    return (
+      <>
+        <meta property="og:url" content={URL} />
+        <meta
+          property="og:title"
+          content={frontMatter.title || "Stoic Handbook"}
+        />
+        <meta
+          property="og:description"
+          content={
+            frontMatter.description ||
+            "Stoic Handbook learn everything you need to know about Stoicism"
+          }
+        />
+      </>
+    );
   },
 };
 
