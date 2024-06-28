817980c506d02f0379f2e98b089f06af876e6d04
Yousaf Wazir <umbrellaman612@gmail.com>
2024-06-28 10:58:50 +0100

    remove filler

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Stoic Handbook</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/UmbrellaCrow612/stoic-handbook",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/UmbrellaCrow612/stoic-handbook/tree/main",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Nextra Docs Template",
   },
-}
+  sidebar: {
+    toggleButton: true,
+  },
+};
 
-export default config
+export default config;
