d67659d99b9756144b8c0a90b55749b487e04b5d
Yousaf Wazir <umbrellaman612@gmail.com>
2024-06-28 13:37:41 +0100

    added more meta and favicons

--- theme.config.tsx
+++ theme.config.tsx
@@ -52,6 +52,48 @@ const config: DocsThemeConfig = {
         />
         <meta name="og:image" content={socialCard} />
         <meta name="twitter:image" content={socialCard} />
+
+        <meta name="msapplication-TileColor" content="#fff" />
+        <meta name="theme-color" content="#fff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta
+          name="description"
+          content={
+            frontMatter.description ||
+            "Stoic Handbook learn everything you need to know about Stoicism"
+          }
+        />
+        <meta
+          name="og:description"
+          content={
+            frontMatter.description ||
+            "Stoic Handbook learn everything you need to know about Stoicism"
+          }
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site:domain" content={origin} />
+        <meta name="twitter:url" content={URL} />
+        <meta
+          name="og:title"
+          content={title ? title + " - Stoic Handbook" : "Stoic Handbook"}
+        />
+        <meta name="apple-mobile-web-app-title" content="Stoic Handbook" />
+        
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.png" type="image/png" />
+        <link
+          rel="icon"
+          href="/favicon-dark.svg"
+          type="image/svg+xml"
+          media="(prefers-color-scheme: dark)"
+        />
+        <link
+          rel="icon"
+          href="/favicon-dark.png"
+          type="image/png"
+          media="(prefers-color-scheme: dark)"
+        />
       </>
     );
   },
