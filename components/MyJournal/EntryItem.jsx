619ded071028c5adc04dcb9227e8d9030de08991
Yousaf Wazir <umbrellaman612@gmail.com>
2024-07-02 11:16:48 +0100

    added edit and delete buttons

--- components/MyJournal/EntryItem.jsx
+++ components/MyJournal/EntryItem.jsx
@@ -1,4 +1,21 @@
+import React from "react";
+
 export const EntryItem = ({ data }) => {
+  const {
+    date,
+    id,
+    morningReflection,
+    eveningReflection,
+    gratitude,
+    improvement,
+    perspective,
+    mementoMori,
+    premeditatioMalorum,
+    voluntaryDiscomfort,
+    nature,
+    emotionalAwareness,
+  } = data;
+
   const renderEntryField = (label, value) => (
     <div className="mb-4">
       <h3 className="text-lg font-semibold mb-2">{label}</h3>
@@ -9,27 +26,33 @@ export const EntryItem = ({ data }) => {
   );
 
   return (
-    <>
-      {" "}
-      <div className="shadow-md rounded-lg p-6 mb-6 border">
-        <div className="flex justify-between items-center mb-4">
-          <h2 className="text-2xl font-bold">{data.date}</h2>
-          <span className="text-sm text-gray-500 dark:text-gray-200">
-            ID: {data.id}
-          </span>
-        </div>
-
-        {renderEntryField("Morning Reflection", data.morningReflection)}
-        {renderEntryField("Evening Reflection", data.eveningReflection)}
-        {renderEntryField("Gratitude", data.gratitude)}
-        {renderEntryField("Self-improvement", data.improvement)}
-        {renderEntryField("Perspective", data.perspective)}
-        {renderEntryField("Memento Mori", data.mementoMori)}
-        {renderEntryField("Premeditatio Malorum", data.premeditatioMalorum)}
-        {renderEntryField("Voluntary Discomfort", data.voluntaryDiscomfort)}
-        {renderEntryField("Nature of the Universe", data.nature)}
-        {renderEntryField("Emotional Awareness", data.emotionalAwareness)}
+    <div className="shadow-md rounded-lg p-6 mb-6 border">
+      <div className="flex justify-between items-center mb-4">
+        <h2 className="text-2xl font-bold">{date}</h2>
+        <span className="text-sm text-gray-500 dark:text-gray-200">
+          ID: {id}
+        </span>
       </div>
-    </>
+
+      {renderEntryField("Morning Reflection", morningReflection)}
+      {renderEntryField("Evening Reflection", eveningReflection)}
+      {renderEntryField("Gratitude", gratitude)}
+      {renderEntryField("Self-improvement", improvement)}
+      {renderEntryField("Perspective", perspective)}
+      {renderEntryField("Memento Mori", mementoMori)}
+      {renderEntryField("Premeditatio Malorum", premeditatioMalorum)}
+      {renderEntryField("Voluntary Discomfort", voluntaryDiscomfort)}
+      {renderEntryField("Nature of the Universe", nature)}
+      {renderEntryField("Emotional Awareness", emotionalAwareness)}
+
+      <div className="flex justify-end mt-4">
+        <button className="bg-blue-500 text-white px-4 py-2 rounded-lg mr-2">
+          Edit
+        </button>
+        <button className="bg-red-500 text-white px-4 py-2 rounded-lg">
+          Delete
+        </button>
+      </div>
+    </div>
   );
 };
